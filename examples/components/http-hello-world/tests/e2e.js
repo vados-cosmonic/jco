92af03a751f58599399fba3cfd5e40bc1b371b1d
Victor Adossi <vadossi@cosmonic.com>
2024-12-02 23:52:41 +0900

    chore: wip
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- examples/components/http-hello-world/tests/e2e.js
+++ examples/components/http-hello-world/tests/e2e.js
@@ -1,7 +1,11 @@
 import { test } from "node:test";
 import { serve } from "bytecodealliance/jco";
 
-test("jco serve basic HTTP request works", () => {
-  await serve("");
+test("jco serve basic HTTP request works", async () => {
+  let args = {};
+  let opts = {};
+  await serve("path/to/component", args, opts);
 
+  // TODO: perform fetch request against component
+  // TODO: might be worth refactoring into a common 'test:e2e' NPM script
 });
