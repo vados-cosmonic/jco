f6e2b2dcf3c149b5071937dcb5dd8fa4bb6b9f16
Victor Adossi <vadossi@cosmonic.com>
2025-08-04 23:15:29 +0900

    wip: fill out more impl

--- packages/jco-std/src/wasi/0.2.x/http/adapters/hono.ts
+++ packages/jco-std/src/wasi/0.2.x/http/adapters/hono.ts
@@ -1,6 +1,19 @@
 import type { Hono, Schema as HonoSchema, Env as HonoEnv } from 'hono';
 
-import { createWebPlatformRequest, writeWasiResponse } from "../types/request.js";
+import { createWebPlatformRequest } from '../types/request.js';
+import { writeWasiResponse } from '../types/response.js';
+
+// TODO: use type bindings for types below
+type IncomingRequest = any;
+type ResponseOutparam = any;
+
+/** Get the global `AddEventListener` */
+function ensureGlobalAddEventListener() {
+    if (!globalThis.addEventListener) {
+        throw new TypeError('AddEventListener not provided by platform');
+    }
+    return globalThis.addEventListener;
+}
 
 /** Strategy for interfacing with WASI environment */
 enum AppAdapterType {
@@ -9,8 +22,12 @@ enum AppAdapterType {
 }
 
 /** Options for building a `AppAdapter` */
-interface AppAdapterOpts {
-    app: Hono;
+interface AppAdapterOpts<
+    Env extends HonoEnv,
+    Schema extends HonoSchema,
+    BasePath extends string,
+> {
+    app: Hono<Env, Schema, BasePath>;
     type?: AppAdapterType;
 }
 
@@ -36,52 +53,93 @@ class AppAdapter<
     #app: Hono<Env, Schema, BasePath>;
 
     /** The Hono App that should be used */
-    #type: AppAdapterType;
+    #adapterType: AppAdapterType;
 
-    constructor(opts: AppAdapterOpts) {
+    constructor(opts: AppAdapterOpts<Env, Schema, BasePath>) {
         if (!opts.app) {
             throw TypeError('Hono app must be provided');
         }
         this.#app = opts.app;
-        this.#type = opts.type ?? AppAdapterType.WasiHTTP;
+        this.#adapterType = opts.type ?? AppAdapterType.WasiHTTP;
+    }
+
+    getAdapterType() {
+        return this.#adapterType;
     }
 
     /**
      * Build an ESM export that represents the app
      */
-    async asESMExport() {
-        switch (this.#type) {
+    asESMExport() {
+        switch (this.#adapterType) {
             // Build an export that would satisfy wasi:http/incoming-handler
             case AppAdapterType.WasiHTTP:
                 return {
                     incomingHandler: {
-                        handle(incomingRequest, responseOutParam) {
-                            const req = createWebPlatformRequest(incomingRequest);
-                            const env = {};
-                            const executionContext = {};
-                            const resp = this.#app.fetch(req, env, executionContext);
-                            writeWasiResponse(resp);
+                        handle(
+                            wasiRequest: IncomingRequest,
+                            wasiResponse: ResponseOutparam
+                        ) {
+                            const env = {}; // TODO: build ENV
+                            const executionContext = {}; // TODO: add useful information in execution context? WASI version?
+                            const resp = this.#app.fetch(
+                                createWebPlatformRequest(wasiRequest),
+                                env,
+                                executionContext
+                            );
+                            writeWasiResponse(resp, wasiResponse);
                         },
                     },
                 };
             // Given that fetch-event is implemented natively for StarlingMonkey,
             // we know that we have already set the handle  already set the we only ahve
             case AppAdapterType.FetchEvent:
-                const { handle } = await import('hono/service-worker');
-                addEventListener('fetch', handle(this.#app));
-                return {};
             default:
-                throw new Error(`unexpected adapter type [${this.#type}]`);
+                throw new Error(
+                    `unexpected adapter type [${this.#adapterType}]`
+                );
         }
     }
 }
+
+/** This global variable will be set to the application adapter when present */
+let ADAPTER: AppAdapter<any, any, any>;
+
+/**
+ * A pre-made incomingHandler export for downstream users to export.
+ *
+ * This export should *not* be used at the top level of a component
+ * that implements `fetch`-based HTTP handlers.
+ */
+export const incomingHandler = {
+    handle(req: IncomingRequest, resp: ResponseOutparam) {
+        if (!ADAPTER) {
+            throw new Error(
+                'app has not been set, ensure fire() was called with your app'
+            );
+        }
+        const adapterType = ADAPTER.getAdapterType();
+        if (adapterType !== AppAdapterType.WasiHTTP) {
+            throw new Error(
+                `invalid adapter type [${adapterType}], expected WASI HTTP`
+            );
+        }
+        const { incomingHandler } = ADAPTER.asESMExport();
+        if (!incomingHandler) {
+            throw new Error(
+                'unexpectedly missing incomingHandler generated ESM export'
+            );
+        }
+        incomingHandler.handle(req, resp);
+    },
+};
 
 /**
  * Serves a Hono application as a `wasi:http/incoming-handler` compliant server
  *
  * @param {Hono} app
  */
-export function serve<
+export function fire<
     Env extends HonoEnv = HonoEnv,
     Schema extends HonoSchema = {},
     BasePath extends string = '/',
@@ -90,6 +148,26 @@ export function serve<
     const adapter = new AppAdapter({
         app,
     });
+
+    const adapterType = adapter.getAdapterType();
+    if (adapterType === AppAdapterType.FetchEvent) {
+        import('hono/service-worker')
+            .then((m) => {
+                const addEventListener = ensureGlobalAddEventListener();
+                addEventListener('fetch', (evt: any) => {
+                    const env = {}; // TODO: build env
+                    const executionContext = {}; // TODO: build execution context
+                    evt.respondWith(app.fetch(evt.request, env));
+                });
+            })
+            .catch((err) => {
+                console.error('failed to build fetch event listener', err);
+                throw err;
+            });
+        return;
+    }
+
+    ADAPTER = adapter;
 
     // TODO: create logger that can be used from every request
     //   - wrap this around wasi:logging
@@ -112,7 +190,4 @@ export function serve<
     //
     // fire({ get: () => console.log("INSIDE APP!") });
     // ```
-
-    throw new Error('not done');
-    return adapter.asESMExport();
 }
