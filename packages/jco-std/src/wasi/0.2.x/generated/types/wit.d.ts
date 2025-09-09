8365d5027b01fe789521bffbe57f85babb9a4ce1
Victor Adossi <vadossi@cosmonic.com>
2025-09-10 02:30:38 +0900

    chore(std): update wit and types

--- packages/jco-std/src/wasi/0.2.x/generated/types/wit.d.ts
+++ packages/jco-std/src/wasi/0.2.x/generated/types/wit.d.ts
@@ -4,6 +4,7 @@
 /// <reference path="./interfaces/wasi-io-error.d.ts" />
 /// <reference path="./interfaces/wasi-io-poll.d.ts" />
 /// <reference path="./interfaces/wasi-io-streams.d.ts" />
+/// <reference path="./interfaces/wasi-logging-logging.d.ts" />
 declare module 'bytecodealliance:jco-std/http-v0m2px' {
   export type * as WasiClocksMonotonicClock024 from 'wasi:clocks/monotonic-clock@0.2.4'; // import wasi:clocks/monotonic-clock@0.2.4
   export type * as WasiHttpIncomingHandler024 from 'wasi:http/incoming-handler@0.2.4'; // import wasi:http/incoming-handler@0.2.4
@@ -11,4 +12,5 @@ declare module 'bytecodealliance:jco-std/http-v0m2px' {
   export type * as WasiIoError024 from 'wasi:io/error@0.2.4'; // import wasi:io/error@0.2.4
   export type * as WasiIoPoll024 from 'wasi:io/poll@0.2.4'; // import wasi:io/poll@0.2.4
   export type * as WasiIoStreams024 from 'wasi:io/streams@0.2.4'; // import wasi:io/streams@0.2.4
+  export type * as WasiLoggingLogging010Draft from 'wasi:logging/logging@0.1.0-draft'; // import wasi:logging/logging@0.1.0-draft
 }
