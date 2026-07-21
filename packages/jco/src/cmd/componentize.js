066330495918d047dea5070ca39e3b4652defeb6
Victor Adossi <vadossi@cosmonic.com>
2026-07-21 21:37:10 +0900

    feat(jco): add shimming to componentize for 'node:path'

--- packages/jco/src/cmd/componentize.js
+++ packages/jco/src/cmd/componentize.js
@@ -3,7 +3,9 @@ import { resolve, basename } from "node:path";
 
 import { componentWitMetadataForWorld } from "@bytecodealliance/jco-transpile";
 
+import { bundleComponentSource, loadBundleConfig } from "../bundle.js";
 import { styleText, isWindows } from "../common.js";
+import { nodeBuiltinPlugin } from "../node-builtins.js";
 
 /** All features that can be enabled/disabled */
 const ALL_FEATURES = ["clocks", "http", "random", "stdio", "fetch-event"];
@@ -18,10 +20,7 @@ const DEBUG_FEATURES = ["stdio"];
  * @param {string} witPath
  * @returns bool
  */
-async function usesOlderWasiHTTP(witPath, worldName) {
-    witPath = (isWindows ? "//?/" : "") + resolve(witPath);
-    const worldMetadata = await componentWitMetadataForWorld({ tag: "path", val: witPath }, worldName ?? null);
-
+function usesOlderWasiHTTP(worldMetadata) {
     // Check if the an old `wasi:http/incoming-handler` version is exported
     const exportsOldIncomingHandler = worldMetadata.exports.some((iface) => {
         return (
@@ -45,18 +44,63 @@ async function usesOlderWasiHTTP(witPath, worldName) {
 
     return exportsOldIncomingHandler || importsOldFetch;
 }
+/**
+ * @typedef {{
+ *   wit: string,
+ *   out: string,
+ *   worldName?: string,
+ *   bundle?: boolean,
+ *   bundleConfig?: string,
+ *   aot?: boolean,
+ *   aotMinStackSizeBytes?: number,
+ *   wevalBin?: string,
+ *   disable?: string[],
+ *   enable?: string[],
+ *   debug?: boolean,
+ *   preview2Adapter?: string,
+ *   debugStarlingmonkeyBuild?: boolean,
+ *   engine?: string,
+ *   debugBindings?: boolean,
+ *   debugBindingsDir?: string,
+ *   debugBinary?: boolean,
+ *   debugBinaryPath?: string,
+ *   debugEnableWizerLogging?: boolean,
+ * }} ComponentizeOptions
+ */
+
+/**
+ * Componentize a JavaScript entry module against a WIT world.
+ *
+ * @param {string} jsSource
+ * @param {ComponentizeOptions} opts
+ */
 export async function componentize(jsSource, opts) {
     const { disableFeatures, enableFeatures } = calculateFeatureSet(opts);
 
-    const source = await readFile(jsSource, "utf8");
+    if (opts.bundleConfig && opts.bundle !== true) {
+        throw new Error("--bundle-config requires --bundle");
+    }
+    const bundleConfig = opts.bundleConfig ? await loadBundleConfig(opts.bundleConfig) : undefined;
     const witPath = resolve(opts.wit);
+    const metadataPath = (isWindows ? "//?/" : "") + witPath;
+    const worldMetadata = await componentWitMetadataForWorld(
+        { tag: "path", val: metadataPath },
+        opts.worldName ?? null,
+    );
+    const source =
+        opts.bundle === true
+            ? await bundleComponentSource(jsSource, {
+                  config: bundleConfig,
+                  plugins: [nodeBuiltinPlugin(worldMetadata)],
+              })
+            : await readFile(jsSource, "utf8");
     const sourceName = basename(jsSource);
 
     // Load an older version of componentize-js if we detect an older version of WASI HTTP in use
     // as the version that is usable is baked into the StarlingMonkey version provided by a given version
     // of componentize-js
     let componentizeJSModule;
-    const useOldComponentizeJS = await usesOlderWasiHTTP(witPath, opts.worldName);
+    const useOldComponentizeJS = usesOlderWasiHTTP(worldMetadata);
     if (useOldComponentizeJS) {
         // NOTE: if we were to use a version of componentize-js 0.20.0 or newer here,
         // the build would fail, as newer versions do not support wasi:http < 0.2.10
